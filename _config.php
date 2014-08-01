6414c8d5257fc11aaf053eb9e75d6e31fdb83cad
Craig Lyons <clyons@execproinc.com>
2014-08-01 09:49:10 -0400

    initial commit

---
+++ _config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('LIQUIDLAYOUT_MODULE', 'liquid_layout');
+
+if (basename(dirname(__FILE__)) != LIQUIDLAYOUT_MODULE) {
+	throw new Exception(LIQUIDLAYOUT_MODULE . ' module not installed in correct directory');
+}
---
+++ code/LiquidLayoutPage.php
@@ -0,0 +1,82 @@
+<?php
+class LiquidLayoutPage extends SiteTree {
+
+    private static $db = array(
+        "TemplateFileName" => "Varchar(255)",
+        "LayoutFileName" => "Varchar(255)"
+    );
+
+    public function getSettingsFields(){
+        $fields = parent::getSettingsFields();
+
+        // Provide DropdownFields for each list retrieved successfully
+        // Otherwise, fall back on TextFields
+        foreach(array("Template", "Layout") as $type)
+        {
+            $DBFieldName = $type."FileName";
+
+            $theField = null;
+            $list = $this->getThemeFileList($type);
+            if($list && count($list) > 0)
+            {
+                $theField = DropdownField::create(
+                    $DBFieldName,
+                    $type,
+                    array_combine($list, $list))
+                    ->setEmptyString("Default (Page)");
+
+            } else {
+                $theField = TextField::create(
+                    $DBFieldName
+                );
+            }
+
+            $fields->addFieldToTab(
+                "Root.Settings",
+                $theField
+            );
+        }
+        return $fields;
+    }
+
+    //Attempts to retrieve list of files from conventional template & Layout dir, trimmed to exclude path & extension
+    private function getThemeFileList($type = "Template")
+    {
+        $themeFileDir = null;
+
+        $baseDir = Director::baseFolder();
+        $themeDir = SSViewer::get_theme_folder();
+        if($baseDir && $themeDir)
+        {
+            $themeFileDir = $baseDir."/".$themeDir."/templates/";
+            if($type == "Layout")
+            {
+                $themeFileDir .= "Layout/";
+            }
+        } else {
+            return null;
+        }
+
+        $fileNameList = glob($themeFileDir."*.ss");
+        $fileNames = array();
+        foreach($fileNameList as $fileName)
+        {
+            $fileNames[] = preg_replace("/\\.[^.\\s]{2,3}$/", "",basename($fileName));
+        }
+
+        if(count($fileNames) > 0){
+            return $fileNames;
+        }
+    }
+}
+
+
+class LiquidLayoutPage_Controller extends ContentController {
+
+    public function index()
+    {
+        $templateFN = $this->TemplateFileName ? $this->TemplateFileName : "Page";
+        $layoutFN = $this->LayoutFileName ? $this->LayoutFileName : "Page";
+        return $this->renderWith(array($layoutFN, $templateFN));
+    }
+}
